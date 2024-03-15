92a912aa1da05f8c9a472597f9049bf2dd242f47
LucasBarbosaSilva <blucas.oficial@gmail.com>
2024-03-15 11:39:51 -0300

    Não mexe

--- tcp_server.py
+++ tcp_server.py
@@ -7,6 +7,7 @@ import time
 host = '127.0.0.1'
 port = 55555
 portas = [5001, 5002, 5003, 5004]
+# portas = [5001, 5002, 5003]
 minha_porta = 5001
 meu_indice = 0
 fila_mensagens = []
@@ -14,6 +15,7 @@ vetor_logico = [0, 0, 0, 0]
 
 clients = []
 names = []
+
 
 def handle_client(client, ):
     while True:
@@ -26,34 +28,63 @@ def handle_client(client, ):
         relogio_recebido = [int(x) for x in relogio_recebido.split(',')]
         vetor_novo = vetor_logico.copy()
         vetor_novo[indice_recebido] += 1
-        print("Vetor novo:", vetor_novo)
-        print("Relogio recebido: ", relogio_recebido)
-        if vetor_novo == relogio_recebido:
+        verificar_fila(vetor_novo)
+        print(vetor_logico)
+        vetor_novo_recebido = [
+            max(a, b)
+            for a, b in zip(
+                vetor_logico, 
+                relogio_recebido    
+            )
+        ]
+        # VA: 0, 0, 1, 1
+        # NVR: 1, 1, 1, 1
+        # VN: 0, 1, 1, 1     
+        vetor_novo = vetor_logico.copy()
+        vetor_novo[indice_recebido] += 1
+        
+        if vetor_novo == vetor_novo_recebido:
+            print("✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓")
             print(f"Mensagem recebida: {mensagem}")
             print(f"Vetor antigo: {vetor_logico}")
-            print(f"Vetor resultante: {vetor_novo}")
-            vetor_logico = vetor_novo.copy()
-            verificar_fila()
+            print(f"Vetor resultante: {vetor_novo_recebido}")
+            vetor_logico = vetor_novo_recebido.copy()
+            verificar_fila(vetor_logico)
         else:
+            print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
             print(f"Mensagem recebida: {mensagem}")
             print(f"Vetores lógicos diferem. A mensagem será guardada em uma fila")
             fila_mensagens.append((mensagem, relogio_recebido, indice_recebido))
+            verificar_fila(vetor_logico)
     client.close()
 
-def verificar_fila():
+def verificar_fila(vetor_logico_temporario):
   global fila_mensagens, vetor_logico, meu_indice
+  print("\n")
+  print("-----------------------------")
+  print("Fila atual: ",fila_mensagens)
+  print("-----------------------------")
   if (fila_mensagens):
     for i, m in enumerate(fila_mensagens):
       mensagem, relogio_recebido, indice_recebido = m
-      vetor_novo = vetor_logico.copy()
+      vetor_novo = vetor_logico_temporario.copy()
+      vetor_novo_recebido = [
+            max(a, b)
+            for a, b in zip(
+                vetor_logico_temporario, 
+                relogio_recebido
+            )
+        ]
       vetor_novo[indice_recebido] += 1
-      if vetor_novo == relogio_recebido:
+      if vetor_novo == vetor_novo_recebido:
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
         print("Mensagem retirada da fila de espera:")
         print(f"Mensagem recebida: {mensagem}")
         print(f"Vetor antigo: {vetor_logico}")
-        print(f"Vetor resultante: {vetor_novo}")
-        vetor_logico = vetor_novo.copy()
+        print(f"Vetor resultante: {vetor_novo_recebido}")
+        vetor_logico = vetor_novo_recebido.copy()
         fila_mensagens.pop(i)
+    
 
 def tcp_chat():
     global vetor_logico, meu_indice
@@ -63,10 +94,8 @@ def tcp_chat():
     print(f"TCP Server listening on port {port}")
 
     while True:
-        print("tcp_chat")
         client, addr = tcp_socket.accept()
         print(f"TCP Connection from {addr}")
-        print(vetor_logico)
         client_handler = threading.Thread(target=handle_client, args=(client,))
         client_handler.start()
 
@@ -82,10 +111,13 @@ def handle_send_message():
     global vetor_logico, meu_indice
     while True:
         vetor_logico[meu_indice] += 1
+        # c = 1
         for porta in portas:
             if porta == port:
                 continue
             send_message('Tchau', porta)
+            # time.sleep(1 + c)
+            # c += 1
         time.sleep(4)
 
 if __name__ == '__main__':
